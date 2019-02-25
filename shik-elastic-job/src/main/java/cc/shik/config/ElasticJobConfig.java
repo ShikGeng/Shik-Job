40ceeee492a8230b2d3e434af2fc3d8fbf050ca6
UCREDIT\gengshikun <gengshikun@youxin.com>
2019-02-25 19:24:59 +0800

    shik-elastic-job

--- shik-elastic-job/src/main/java/cc/shik/config/ElasticJobConfig.java
+++ shik-elastic-job/src/main/java/cc/shik/config/ElasticJobConfig.java
@@ -37,7 +37,7 @@ public class ElasticJobConfig {
 
     private LiteJobConfiguration getLiteJobConfiguration(final Class<? extends SimpleJob> jobClass, final String cron, final int shardingTotalCount, final String shardingItemParameters) {
         return LiteJobConfiguration.newBuilder(new SimpleJobConfiguration(JobCoreConfiguration.newBuilder(
-                jobClass.getName(), cron, shardingTotalCount).shardingItemParameters(shardingItemParameters).build(), jobClass.getCanonicalName())).overwrite(true).build();
+                jobClass.getName(), cron, shardingTotalCount).shardingItemParameters(shardingItemParameters).build(), jobClass.getCanonicalName())).overwrite(true).disabled(true).build();
     }
 
     @Bean(initMethod = "init")
